0050c69325a2a813812f67ed7a2d87182f012724
Riccardo Cardia <cardiariccardo94@gmail.com>
2026-03-12 18:13:05 +0100

    aggiunto slider e moduli

---
+++ script01.js
@@ -0,0 +1,28 @@
+const container = document.getElementById("row");
+const spinner = document.getElementById("spinner");
+
+export async function fetchData() {
+  const response = await fetch("./file.json");
+  const data = await response.json();
+  return data;
+}
+
+export const populatediv = async function () {
+  spinner.classList.remove("d-none");
+  const data = await fetchData();
+  spinner.classList.add("d-none");
+  data.forEach((card) => {
+    const cards = document.createElement("div");
+    cards.classList.add("col-4", "my-4");
+    cards.innerHTML = `
+    <div class="card col-4" style="width: 18rem;">
+      <div class="card-body">
+      <h5 class="card-title">${card.nome}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">${card.prezzo}</h6>
+      <p class="card-text">${card.categoria}</p>
+      </div>
+      </div>
+      `;
+    container.appendChild(cards);
+  });
+};
